993d3d812dd8e546c270b447494e1a8f13aa7ceb
faizanarhamsoft2 <faizan.zahid@arhamsoft.org>
2024-01-08 16:21:38 +0500

    Update login.js

--- automaton/login.js
+++ automaton/login.js
@@ -4,6 +4,18 @@ const puppeteer = require('puppeteer');
 const { sleep } = require('../utils/utils');
 
 const login = async (username, password) => {
+    // Check if an XPath exists on the page?.
+    const XPathExists = async (XPath) => {
+        try {
+            if (await page?.waitForXPath(XPath, { timeout: 2500 })) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (err) {
+            return false;
+        }
+    };
     const browser = await puppeteer.launch({
         headless: false,
         defaultViewport: null,
@@ -12,24 +24,24 @@ const login = async (username, password) => {
     });
 
     const page = await browser.newPage();
-    if(process.env.USE_USER_AGENT == "true"){
+    if (process.env.USE_USER_AGENT == "true") {
         await page.setUserAgent('Mozilla/5.0 (Linux; Android 13; SM-A037U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36 uacq');
     }
     // Open the Facebook Login Page.
     try {
-        const loginURL = 'https://www.facebook.com/login';
+        const loginURL = 'https://m.facebook.com/login';
 
         await page.goto(loginURL);
         await page.waitForNetworkIdle();
+        await sleep(2000)
+        // if ((await page.url()) !== loginURL) {
+        //     await browser.close();
 
-        if ((await page.url()) !== loginURL) {
-            await browser.close();
-
-            return {
-                success: true,
-                error: null,
-            };
-        }
+        //     return {
+        //         success: true,
+        //         error: null,
+        //     };
+        // }
     } catch (err) {
         if (err) {
             await browser.close();
@@ -45,61 +57,91 @@ const login = async (username, password) => {
             };
         }
     }
+    // If there is an authentication required to account 
+    if (await XPathExists("/html/body/div[1]/div/div[3]/div[1]/div/div[2]/div/div[3]/form/div[4]/div[1]/div/div/input")) {
+        try {
+            const emailField = await page.$('input[name="email"]');
+            await emailField.type(process.env.FB_USERNAME);
 
+            const passwordField = await page.$('input[name="pass"]');
+            await passwordField.type(process.env.FB_PASSWORD);
+
+            await page.keyboard.press('Escape');
+
+            const [login] = await page?.$x(`//span[text()='Log In']`);
+            await login.evaluate((s) => s.click());
+            await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+            await sleep(4000)
+        } catch (err) {
+            if (err) {
+                // await browser?.close();
+                return {
+                    success: false,
+                    data: null,
+                    error: {
+                        code: 702,
+                        type: 'Puppeteer error.',
+                        moment: 'Authenticating Meta Composer.',
+                        error: err?.toString(),
+                    },
+                };
+            }
+        }
+    }
     // Fill in the login details
-    try {
-        const usernameField = 'input[name=email][type=text][autocomplete=username]';
-        const passwordField = 'input[type=password][name=pass][autocomplete="current-password"]';
+    // try {
+    //     const usernameField = 'input[name=email][type=text][autocomplete=username]';
+    //     const passwordField = 'input[type=password][name=pass][autocomplete="current-password"]';
 
-        await page.waitForSelector(usernameField);
-        await page.click(usernameField);
-        await page.type(usernameField, username, { delay: 25 });
+    //     await page.waitForSelector(usernameField);
+    //     await page.click(usernameField);
+    //     await page.type(usernameField, username, { delay: 25 });
 
-        await page.waitForSelector(passwordField);
-        await page.click(passwordField);
-        await page.type(passwordField, password, { delay: 25 });
-    } catch (err) {
-        if (err) {
-            await browser.close();
-            return {
-                success: false,
-                error: {
-                    code: 702,
-                    type: 'Puppeteer error.',
-                    moment: 'Filling in the user login details.',
-                    message: err.toString(),
-                },
-            };
-        }
-    }
+    //     await page.waitForSelector(passwordField);
+    //     await page.click(passwordField);
+    //     await page.type(passwordField, password, { delay: 25 });
+    // } catch (err) {
+    //     if (err) {
+    //         await browser.close();
+    //         return {
+    //             success: false,
+    //             error: {
+    //                 code: 702,
+    //                 type: 'Puppeteer error.',
+    //                 moment: 'Filling in the user login details.',
+    //                 message: err.toString(),
+    //             },
+    //         };
+    //     }
+    // }
 
-    // Proceed with the login sequence
-    try {
-        const loginButton = 'button#loginbutton[name=login][type=submit]';
+    // // Proceed with the login sequence
+    // try {
+    //     const loginButton = 'button#loginbutton[name=login][type=submit]';
 
-        await page.waitForSelector(loginButton);
-        await page.click(loginButton);
-    } catch (err) {
-        if (err) {
-            await browser.close();
+    //     await page.waitForSelector(loginButton);
+    //     await page.click(loginButton);
+    // } catch (err) {
+    //     if (err) {
+    //         await browser.close();
 
-            return {
-                success: false,
-                error: {
-                    code: 703,
-                    type: 'Puppeteer error.',
-                    moment: 'Proceeding with the login sequence.',
-                    message: err.toString(),
-                },
-            };
-        }
-    }
+    //         return {
+    //             success: false,
+    //             error: {
+    //                 code: 703,
+    //                 type: 'Puppeteer error.',
+    //                 moment: 'Proceeding with the login sequence.',
+    //                 message: err.toString(),
+    //             },
+    //         };
+    //     }
+    // }
 
     // Check if the user was stopped by the checkpoint
     try {
         await sleep(5000);
 
-        const checkpointURL = 'https://www.facebook.com/checkpoint/?next';
+        const checkpointURL = 'https://m.facebook.com/checkpoint/?next';
 
         await sleep(3000);
 
@@ -127,10 +169,10 @@ const login = async (username, password) => {
         const pageURL = await page.url();
 
         if (
-            pageURL === 'https://www.facebook.com/home.php' ||
-            pageURL === 'https://www.facebook.com/home.php/' ||
-            pageURL === 'https://www.facebook.com' ||
-            pageURL === 'https://www.facebook.com/'
+            pageURL === 'https://m.facebook.com/home.php' ||
+            pageURL === 'https://m.facebook.com/home.php/' ||
+            pageURL === 'https://m.facebook.com' ||
+            pageURL === 'https://m.facebook.com/'
         ) {
             await sleep(5000);
             await browser.close();
