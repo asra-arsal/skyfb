e59e29f7ad977bb8f517cdeff0e8b2df3fe90c58
faizanarhamsoft2 <faizan.zahid@arhamsoft.org>
2024-01-22 12:46:42 +0500

    Update login.js

--- automaton/login.js
+++ automaton/login.js
@@ -20,7 +20,7 @@ const login = async (username, password) => {
     const username_ = process.env.PROXY_USER;
     const password_ = process.env.PROXY_PASSWORD;
     let args = ['--start-maximized', '--disable-notifications',]
-    if (process.env.PROXY_ENABLED === "true") {
+    if (["true",true].includes(process.env.PROXY_ENABLED) ) {
         args.push(`--proxy-server=${proxy}`)
     }
     const browser = await puppeteer.launch({
@@ -33,13 +33,13 @@ const login = async (username, password) => {
     const page = await browser.newPage();
     await sleep(3000)
 
-    if (process.env.PROXY_ENABLED === "true") {
+    if (["true",true].includes(process.env.PROXY_ENABLED)) {
         console.log('Proxy is enabled')
         await page.authenticate({ username_, password_ });
         await sleep(2000)
     }
-    if (process.env.USE_USER_AGENT == "true") {
-        await page.setUserAgent('Mozilla/5.0 (Linux; Android 13; SM-A037U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36 uacq');
+    if (process.env.USE_USER_AGENT) {
+        await page.setUserAgent(USE_USER_AGENT);
     }
     if(["true", true].includes(process.env.PROXY_ENABLED)){
         // Open ipinfo.
