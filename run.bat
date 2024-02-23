b1e40dfa3d8dea3994862299306dab551006ca2a
Asra & Arsal <126740113+asra-arsal@users.noreply.github.com>
2024-02-23 22:22:43 +0100

    Create run.bat
    run

---
+++ run.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "currentPath=%~dp0"
+
+for %%A in ("%currentPath%") do set "currDir=%%~nxA"
+
+set "parentPath=%currentPath%\.."
+
+for %%A in ("%parentPath%") do set "prevDir=%%~nxA"
+
+
+set "grandParentPath=%parentPath%\.."
+
+for %%A in ("%grandParentPath%") do set "grandParentDir=%%~nxA"
+
+set "cmd=pm2 start main.js -n !grandParentDir!-!prevDir!"
+
+echo Command: !cmd!
+
+!cmd!
